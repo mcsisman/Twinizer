1abed3386e724dbc27e4fa6f44198c1d34c67c96
mcsisman <m.cemil.sisman@gmail.com>
2021-02-03 12:28:23 +0300

    font

--- source/Login/Login.js
+++ source/Login/Login.js
@@ -110,7 +110,10 @@ Login = (email, password) => {
   }
 
   render(){
-
+    var fontFam;
+    if(Platform.OS == "android"){
+      fontFam = ""
+    }
     const {navigate} = this.props.navigation;
     var lang = language[global.lang]
     var keyboardAvoidingHeight;
@@ -166,7 +169,7 @@ Login = (email, password) => {
           keyboardType= "email-address"
           onFocus = {()=>{console.log("keyboardavoid:", keyboardAvoidingHeight)
          this.setState({keyboardOpen: true})}}
-          style={{ fontFamily: Platform.OS == "android" ? "" : "San Francisco", paddingLeft: 0, paddingBottom:this.width/50, fontSize: 17*(this.width/360), width: this.width*(6/10),
+          style={{ fontFamily: fontFam, paddingLeft: 0, paddingBottom:this.width/50, fontSize: 17*(this.width/360), width: this.width*(6/10),
            borderColor: 'rgba(241,51,18,0)', borderBottomColor: 'white', borderBottomWidth: 1}}
            onChangeText={(text) => this.setState({isim: text})}>
           </TextInput>
@@ -179,7 +182,7 @@ Login = (email, password) => {
           secureTextEntry
           onFocus = {()=>{console.log("keyboardavoid:", keyboardAvoidingHeight)
          this.setState({keyboardOpen: true})}}
-          style={{fontFamily: Platform.OS == "android" ? "" : "San Francisco", paddingLeft: 0, paddingBottom: this.width/50, fontSize: 17*(this.width/360), width: this.width*(6/10),
+          style={{fontFamily: fontFam, paddingLeft: 0, paddingBottom: this.width/50, fontSize: 17*(this.width/360), width: this.width*(6/10),
            backgroundColor: 'rgba(255,255,255,0)', borderColor: 'rgba(241,51,18,0)', borderBottomColor: 'white', borderBottomWidth: 1}}
            onChangeText={(text) => this.setState({sifre: text})}>
         </TextInput>
@@ -191,7 +194,7 @@ Login = (email, password) => {
         style={{justifyContent: 'center', position: 'absolute', backgroundColor: 'rgba(255,255,255, 0)',
           paddingLeft: 15, paddingRight: 15, height: (this.height*6)/100, flex:1}}
         onPress={()=>this.check() }>
-        <Text style={{fontFamily: Platform.OS == "android" ? "" : "San Francisco", textAlign: 'center', color: 'white', fontSize: 17*(this.width/360)}}>
+        <Text style={{fontFamily: fontFam, textAlign: 'center', color: 'white', fontSize: 17*(this.width/360)}}>
           {lang.Login}
         </Text>
         </TouchableOpacity>
@@ -205,7 +208,7 @@ Login = (email, password) => {
          padding:this.width/50, bottom: 0, right: this.width*(2/100)}}
          onPress={()=>navigate("NewAccount") }>
         <Text style={{
-          fontFamily: Platform.OS == "android" ? "" : "San Francisco",
+          fontFamily: fontFam,
           textAlign: 'right',
           color: 'white',
           fontSize: 18*(this.width/360)
@@ -219,7 +222,7 @@ Login = (email, password) => {
         style={{position: 'absolute',
          padding:this.width/50, bottom: 0, left: this.width*(2/100)}}
          onPress={()=> navigate( "ForgotPassword") }>
-        <Text style={{fontFamily: Platform.OS == "android" ? "" : "San Francisco", textAlign: 'left', color: 'white', fontSize: 18*(this.width/360)}}>
+        <Text style={{fontFamily: fontFam, textAlign: 'left', color: 'white', fontSize: 18*(this.width/360)}}>
           {lang.ForgotPassword}
         </Text>
         </TouchableOpacity>
