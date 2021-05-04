f4fcab77830dcdbdb75fca46be3e8700d824c6ee
mcsisman <m.cemil.sisman@gmail.com>
2021-05-04 06:48:49 +0300

    dp ios

--- source/Components/ProfileSteps/UserInfo/DatePickerIOSModal.js
+++ source/Components/ProfileSteps/UserInfo/DatePickerIOSModal.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {createStackNavigator, Header} from 'react-navigation-stack';
 import Modal from "react-native-modal";
 import PropTypes from 'prop-types';
+import language from '../Utils/Languages/lang.json'
+import ImgModalOvalButton from '../../Common/ImageUpload/ImgModalOvalButton'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import {
   View,
@@ -28,6 +30,7 @@ export default class DatePickerIOSModal extends Component {
     super(props);
     this.state = {
       date: new Date(),
+      dateSet: false,
     }
   }
 
@@ -39,10 +42,11 @@ export default class DatePickerIOSModal extends Component {
  static defaultProps = {
  }
 
- getInfo(dateSet){
-
+ updateState(dateSet, date){
+   this.props.updateDateState(dateSet, date);
  }
   render(){
+    var lang = language[global.lang]
     this.height = Math.round(Dimensions.get('screen').height);
     this.width = Math.round(Dimensions.get('screen').width);
     console.log("render date picker ios modal")
@@ -56,15 +60,13 @@ export default class DatePickerIOSModal extends Component {
       coverScreen = {false}
       deviceHeight = {screenHeight}
       deviceWidth = {screenWidth}
-      animationIn = "zoomInUp"
-      animationOut = "zoomOutUp"
       animationInTiming = {500}
       animationOutTiming = {500}
       isVisible={this.props.isVisible}>
       <View style={{
       borderRadius: 8, borderWidth: 0.3, borderColor: "rgba(0,0,0,4)",
       backgroundColor: global.isDarkMode ? global.darkModeColors[1] : "rgba(255,255,255,1)",
-      width: screenWidth*(7.5/10),
+      width: screenWidth*(8/10),
       paddingTop: 10,
       paddingBottom: 15,
       alignItems: 'center',
@@ -73,27 +75,50 @@ export default class DatePickerIOSModal extends Component {
       bottom: screenHeight*(1/10)}}>
 
       <DateTimePicker
-      style = {{ width: 300, opacity: 1, height: 300, marginTop: 50}}
+      textColor = {global.themeColor}
+      style = {{ width: screenWidth*(8/10), opacity: 1, height:screenWidth*(6/10)}}
         value={this.state.date}
         display = "spinner"
         minimumDate= {new Date("1921-01-01")}
         maximumDate={new Date(this.props.maxDate)}
         onChange={(event, date) => {
-          if(event.type == "set"){
-            /*/if(date == null || global.globalGender == "" || global.globalGender == null || global.globalCountry == null || global.globalCountry == ""){
-              this.setState({dateSet: true, showDatePicker: false, disabled: true, dateTextColor: global.themeColor, date: date, buttonOpacity: global.themeColor, opacity: 0.4})
-            }
-            else{
-              this.setState({dateSet: true, showDatePicker: false, disabled: false, dateTextColor: global.themeColor, date: date, buttonOpacity: global.themeColor, opacity: 1})
-            }
-            global.globalBirthday = date*/
-          }
-          else{
-            console.log("________________SET DEĞİLLL______________")
-          }
-
+          this.setState({dateSet: true, date: date, })
+          console.log("________________SEÇİLEN TARİH______________", this.state.date)
         }}
       />
+
+      <View
+
+      style={{ flexDirection: 'row', width: screenWidth*(78/10), height:screenWidth*(1.5/10), justifyContent: 'center',
+      paddingLeft: 15, paddingRight: 15}}>
+
+      <View
+      style={{ width: screenWidth*(4/10), height:screenWidth*(1.5/10), justifyContent: 'center', alignItems:'center'}}>
+      <ImgModalOvalButton
+      activeOpacity = {0.7}
+      title = {lang.Cancel}
+      textColor = {global.themeColor}
+      onPress = {()=>this.updateState(this.state.dateSet, this.state.date))}
+      borderColor = {'rgba(241,51,18,0)'}
+      borderRadius = {0}
+      textFontSize = {20*(screenWidth/360)}
+      borderWidth = {0}/>
+      </View>
+
+      <View
+      style={{width: screenWidth*(4/10), height:screenWidth*(1.5/10), justifyContent: 'center', alignItems:'center'}}>
+      <ImgModalOvalButton
+      activeOpacity = {0.7}
+      title = {lang.OK}
+      textColor = {global.themeColor}
+      onPress = {()=>this.updateState(this.state.dateSet, this.state.date))}
+      borderColor = {'rgba(241,51,18,0)'}
+      borderRadius = {0}
+      textFontSize = {20*(screenWidth/360)}
+      borderWidth = {0}/>
+      </View>
+
+      </View>
 
       </View>
       </Modal>
